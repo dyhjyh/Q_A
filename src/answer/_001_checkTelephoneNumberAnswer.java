9fa032172730ccacf6b87d1b81273f1cd429b223
DYH <1245687900@qq.com>
2022-01-25 12:20:21 +0800

    001

--- src/answer/_001_checkTelephoneNumberAnswer.java
+++ src/answer/_001_checkTelephoneNumberAnswer.java
@@ -8,16 +8,29 @@ public class _001_checkTelephoneNumberAnswer {
     
     public boolean check(String telephoneNumber) {
         /* 001_1:
-        if (telephoneNumber.length() == 11 && telephoneNumber.charAt(0) == '1') {
-            try {
-                Long.parseLong(telephoneNumber);
-            } catch (NumberFormatException e) {
-                return false;
+            return telephoneNumber.matches("1[0-9]{10}");
+         */
+        
+        /* 001_2:
+            if (telephoneNumber.length() == 11 && telephoneNumber.charAt(0) == '1') {
+                try {
+                    Long.parseLong(telephoneNumber);
+                } catch (NumberFormatException e) {
+                    return false;
+                }
+                return true;
             }
+            return false;
+         */
+        
+        /* 001_3:(HHB)
+            if(telephoneNumber.length()!=11||telephoneNumber.length()==0)return false;
+            char[] midstr=telephoneNumber.toCharArray();
+            if(midstr[0]!='1')return false;
+            for(int i=telephoneNumber.length()-1;i>1;i--)
+                if(!Character.isDigit(midstr[i]))return false;
             return true;
-        }
-        return false;
-        */
+         */
         return false;
     }
 }
